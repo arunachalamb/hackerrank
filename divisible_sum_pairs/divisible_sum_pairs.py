fcdb66c06500fa0f6880f6d07d596a0356d8521c
Babu Arunachalam <arunachalamb@users.noreply.github.com>
2017-11-03 18:08:37 -0700

    Divisible sum pairs
    Given an array of n integers and a positive integer k, find all pairs
    a[i] + a[j] where i < j and divisible by k.

---
+++ divisible_sum_pairs/divisible_sum_pairs.py
@@ -0,0 +1,15 @@
+import sys
+
+def divisibleSumPairs(n, k, ar):
+    count = 0
+    for i in range(0, len(ar)):
+        for j in range(i+1, len(ar)):
+            if (ar[i] + ar[j]) % k == 0:
+                count += 1
+    return count
+
+n, k = input().strip().split(' ')
+n, k = [int(n), int(k)]
+ar = list(map(int, input().strip().split(' ')))
+result = divisibleSumPairs(n, k, ar)
+print(result)
