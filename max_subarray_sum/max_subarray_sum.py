d5d6f278005849d6f98b3d23d8882ae11a61bff2
Babu Arunachalam <babu.arunachalam@vizexplorer.com>
2018-02-14 22:07:51 -0800

    Max subarray sum
    Find the max subarray sum module m for a given array a and integer m.

---
+++ max_subarray_sum/max_subarray_sum.py
@@ -0,0 +1,21 @@
+import sys
+
+
+def maximumSum(a, m):
+    mx = 0
+    for i in range(1, len(a)):
+        b = [sum(a[j:j+i])%m for j in range(len(a)-i+1)]
+        x = max(b)
+        if x > mx:
+            mx = x
+    return mx
+    # Complete this function
+
+if __name__ == "__main__":
+    q = int(input().strip())
+    for a0 in range(q):
+        n, m = input().strip().split(' ')
+        n, m = [int(n), int(m)]
+        a = list(map(int, input().strip().split(' ')))
+        result = maximumSum(a, m)
+        print(result)
